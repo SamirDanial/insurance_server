537abbb1f033bda68cc11c0dc7d55d1394f635be
Consult Things <samirdanial7@gmail.com>
2022-01-19 10:45:25 +0500

    small changes

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.use(
 );
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('Client/build'));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
 }
 
