31a98e8b60d09ac7b41431151672145e0f5195ea
Consult Things <samirdanial7@gmail.com>
2022-01-19 22:52:04 +0500

    another changes into build folder

--- server.js
+++ server.js
@@ -52,12 +52,11 @@ app.use(
   })
 );
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  })
-}
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 
