4ef663199b490a4eee3b8fe5eb468026783fade7
Consult Things <samirdanial7@gmail.com>
2022-01-19 10:42:26 +0500

    static files added

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.use(
 );
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('Client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
   })
 }
 
